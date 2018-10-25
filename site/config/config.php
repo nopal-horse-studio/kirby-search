d6e76fc937926b7e9223bf61dab7e588102290f9
Jorge Fco <jorge.fco.mend@gmail.com>
2018-10-25 16:03:52 -0500

    Add file config

---
+++ site/config/config.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+---------------------------------------
+License Setup
+https://getkirby.com/docs/cheatsheet/options/license
+---------------------------------------
+*/
+c::set('license', 'put your license key here');
+
+/*
+---------------------------------------
+Kirby Configuration
+https://getkirby.com/docs/cheatsheet/options/debug
+---------------------------------------
+*/
+c::set('debug', true);
+
+/*
+---------------------------------------
+Kirby Home
+https://getkirby.com/docs/cheatsheet/options/home
+---------------------------------------
+*/
+c::set('home','search');
